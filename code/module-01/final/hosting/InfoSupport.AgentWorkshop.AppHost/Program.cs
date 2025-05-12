74aa4af12ba66e737db9d0f4187d667f3043b449
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-05-12 21:51:34 +0200

    Fix starter code for module 1 so it includes the indexer

--- code/module-01/final/hosting/InfoSupport.AgentWorkshop.AppHost/Program.cs
+++ code/module-01/final/hosting/InfoSupport.AgentWorkshop.AppHost/Program.cs
@@ -5,12 +5,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgresServer = builder.AddPostgres("postgres").WithDataVolume();
 var chatDatabase = postgresServer.AddDatabase("chatservicedb", "chat");
 
+var qdrantServer = builder.AddQdrant("qdrant").WithDataVolume();
+
 var languageModel = builder.AddConnectionString("languagemodel");
 
-var chatService = builder.AddProject<InfoSupport_Ricardo_Chat>("chatservice")
+var chatService = builder.AddProject<InfoSupport_AgentWorkshop_Chat>("chatservice")
     .WithReference(chatDatabase)
     .WithReference(languageModel)
     .WaitFor(chatDatabase);
+
+var indexerService = builder.AddProject<InfoSupport_AgentWorkshop_Indexer>("indexerservice")
+    .WithReference(qdrantServer)
+    .WithReference(languageModel)
+    .WaitFor(qdrantServer);
+
 
 var frontend = builder.AddNpmApp("frontend", "../../apps/frontend", "dev")
     .WithExternalHttpEndpoints()
